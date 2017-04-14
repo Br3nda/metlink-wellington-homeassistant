69368c65328bd9184e19708353fbf8d2a772f663
Brenda Wallace <brenda@wallace.net.nz>
2017-04-14 17:15:31 +1200

    Show arrivals of multiple stops, by route_number

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
+## Show the RTI (real time info) expected arrival time of the next bus
 
 Example config:
 ```
 sensor:
   - platform: metlink
-    stop_number: 5020
+    stop_number: 1000
+    route_number: 1
+  - platform: metlink
+    stop_number: 1000
+    route_number: 10
+  - platform: metlink
+    stop_number: 1000
+    route_number: 32
+
 ```
--- metlink.py
+++ metlink.py
@@ -17,27 +17,10 @@ from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
-_RESOURCE = 'http://transport.opendata.ch/v1/'
-
-ATTR_DEPARTURE_TIME1 = 'next_departure'
-ATTR_DEPARTURE_TIME2 = 'next_on_departure'
-ATTR_REMAINING_TIME = 'remaining_time'
-ATTR_START = 'start'
-ATTR_TARGET = 'destination'
 
 CONF_ATTRIBUTION = "Data provided by metlink.org.nz"
-
-DEFAULT_NAME = 'Next Departure'
 ICON = 'mdi:bus'
-
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
-TIME_STR_FORMAT = "%H:%M"
-
-# PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-#     vol.Required('stop_numbers'): cv.arry,
-#     vol.Required(CONF_START): cv.string,
-#     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-# })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -45,65 +28,165 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     # journal contains [0] Station ID start, [1] Station ID destination
     # [2] Station name start, and [3] Station name destination
     stop_number = config.get('stop_number')
-    add_devices([MetlinkSensor(stop_number)])
+    route_number = config.get('route_number')
+    add_devices([MetlinkSensor(stop_number, route_number)])
+
+
+def service_summary(service):
+    realtime = "(sched)"
+    if service.is_real_time:
+        realtime = "(real)"
+
+    return "{} {} to {}".format(
+        realtime,
+        service.origin_stop_name,
+        service.destination_stop_name,
+    )
+
+
+def pretty_timestamp(t):
+    #
+    # python2's strptime doesn't handle %z so here's a workaround, from:
+    #   http://stackoverflow.com/questions/1101508/how-to-parse-dates-with-0400-timezone-string-in-python/23122493#23122493
+    #
+    ret = datetime.strptime(t[0:16], '%Y-%m-%dT%H:%M')
+    if t[18] == '+':
+        ret += timedelta(hours=int(t[19:22]), minutes=int(t[23:]))
+    elif t[18] == '-':
+        ret -= timedelta(hours=int(t[19:22]), minutes=int(t[23:]))
+
+    return ret.strftime("%I:%M%p %a")
 
 
 class MetlinkSensor(Entity):
     """Implementation of an Metlink public transport sensor."""
 
-    def __init__(self, stop_number):
+    def __init__(self, stop_number, route_number):
         """Initialize the sensor."""
         self.stop_number = stop_number
-        self._name = 'stop_{stop_number}'.format(stop_number=stop_number)
+        self.route_number = route_number
+        self._name = 'stop_{stop_number}_route_{route_number}'.format(
+            stop_number=stop_number,
+            route_number=route_number
+        )
         self.update()
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name
+        return "Route {}".format(self.route_number)
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self._data.get('Services')[0].get('DepartureStatus')
+        # return self.metlink_stop.services[0].departure_status
+        service = self.metlink_stop.next_service(
+            route_number=self.route_number)
+        if service.is_real_time:
+            return int(service.departure_seconds / 60)
+        else:
+            return service.DisplayDeparture
 
     @property
     def device_state_attributes(self):
         """Return the state attributes."""
-        """
-        {'IsRealtime': True, 'Direction': 'Inbound', 'ExpectedDeparture': '2017-04-13T22:33:39+12:00',
-        'Service': {'Code': '11', 'TrimmedCode': '11', 'Link': '/timetables/bus/11', 'Mode': 'Bus',
-        'Name': 'Seatoun - Wellington'}, 'AimedDeparture': '2017-04-13T22:31:00+12:00',
-        'DestinationStopID': '5016', 'OriginStopName': 'SeatounPk-HectorSt',
-        'OriginStopID': '7042', 'ServiceID': '11',
-        'AimedArrival': '2017-04-13T22:31:00+12:00',
-        'VehicleFeature': 'lowFloor', 'VehicleRef': '1427',
-        'DepartureStatus': 'delayed', 'OperatorRef': 'NZBS',
-        'DisplayDepartureSeconds': 321,
-        'DestinationStopName': 'Wgtn Station', 'DisplayDeparture': '2017-04-13T22:33:39+12:00'}
+        return self.next_service.attributes()
 
-        """
-        attributes = {'name': self._stop_info.get('Name')}
-        for service in self._data.get('Services'):
-            _LOGGER.info(service)
-            service_label = "{service_id} {service_name}".format(
-                service_id=service.get('Service').get('Code'),
-                service_name=service.get('Service').get('Name')
-            )
-            service_time = datetime.strptime(service.get(
-                'AimedDeparture'), "%Y-%m-%dT%H:%M:%S+12:00")
-            attributes[service_time.strftime('%H:%M %a')] = service_label
-        return attributes
+    @property
+    def next_service(self):
+        return self.metlink_stop.next_service(route_number=self.route_number)
 
     @property
     def icon(self):
         """Icon to use in the frontend, if any."""
         return ICON
 
+    @property
+    def unit_of_measurement(self):
+        """Return the unit this state is expressed in."""
+        if self.next_service.is_real_time:
+            return "min"
+        else:
+            return "offline"
+
     def update(self):
-        """Get the latest data from opendata.ch and update the states."""
+        """Get the latest data from Metlink and update the states."""
         url = "https://www.metlink.org.nz/api/v1/StopDepartures/{stop_number}".format(
             stop_number=self.stop_number)
         r = requests.get(url)
-        self._data = r.json()
-        self._stop_info = self._data.get('Stop')
+        self.metlink_stop = MetlinkStop(r.json())
+
+
+class MetlinkStop(object):
+    """ A bus stop or train station """
+
+    def __init__(self, data):
+        self._data = data
+
+    def services(self, route_number=None):
+        for data in self._data.get('Services'):
+            service = MetlinkService(data)
+            if not route_number or (route_number and str(route_number) == str(service.route_number)):
+                yield service
+
+    @property
+    def stop_name(self):
+        return self._data.get('Stop', {}).get('Name')
+
+    def next_service(self, route_number=None):
+        """The next service expected to arrive here, optionally filtered by route_number """
+        for service in self.services(route_number=route_number):
+            return service
+
+
+class MetlinkService(object):
+    """ A single service, e.g. Bus on route 1, from bus stop 1000, traveling the whole route """
+
+    def __init__(self, service_data):
+        self._service_data = service_data
+
+    @property
+    def route_number(self):
+        return self._get('ServiceID')
+
+    @property
+    def departure_status(self):
+        return self._get('DepartureStatus')
+
+    @property
+    def display_departure(self):
+        return self._get('DisplayDeparture')
+
+    @property
+    def is_real_time(self):
+        return self._get('IsRealtime')
+
+    @property
+    def origin_stop_name(self):
+        return self._get('OriginStopName')
+
+    @property
+    def destination_stop_name(self):
+        return self._get('DestinationStopName')
+
+    @property
+    def departure_seconds(self):
+        return self._get('DisplayDepartureSeconds')
+
+    def attributes(self):
+        return {
+            'Operator': self._get('OperatorRef'),
+            'ExpectedDeparture': pretty_timestamp(self._get('ExpectedDeparture')),
+            'DepartureStatus': self._get('DepartureStatus'),
+            'IsRealtime': self._get('IsRealtime'),
+            'OriginStopName': self._get('OriginStopName'),
+            'DestinationStopName': self._get('DestinationStopName'),
+            'VehicleFeature': self._get('VehicleFeature'),
+            'ServiceID': self._get('ServiceID')
+        }
+
+    def _get(self, key):
+        return self._service_data.get(key)
+
+    def __str__(self):
+        return str(self._service_data)
